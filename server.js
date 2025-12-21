2ccd987008b79d4e3989b4fa7ce7ba01e50aba09
unknown <dhiyaulhaqfakhrimohammad@gmail.com>
2025-12-21 10:38:39 +0700

    update user query

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +14,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use("/categories", categoryRoutes);
 app.use("/products", productRoutes);
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
